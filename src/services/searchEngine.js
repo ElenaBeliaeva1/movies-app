0846a31aa06e2d00e41470e92b1e0548b368dcd4
ElenaBeliaeva1 <elena.belyaeva2003@mail.ru>
2023-03-14 13:13:37 +0300

    first commit

---
+++ src/services/searchEngine.js
@@ -0,0 +1,76 @@
+import { render } from '@testing-library/react'
+import { Alert } from 'antd'
+
+export default class SearchEngine {
+  async getGuestToken() {
+    const result = fetch(
+      'https://api.themoviedb.org/3/authentication/guest_session/new?api_key=297a876b5d898a21bdf4174dff371b61'
+    )
+    const { guest_session_id } = await result.json()
+    localStorage.setItem('guestToken', guest_session_id)
+  }
+
+  async rateMovie(stars, movieId) {
+    await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/rating?api_key=297a876b5d898a21bdf4174dff371b61&guest_session_id=${localStorage.getItem(
+        'guestToken'
+      )}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify({ value: stars }),
+      }
+    )
+  }
+
+  async getRatedMovies(page) {
+    const result = await fetch(
+      `https://api.themoviedb.org/3/guest_session/${localStorage.getItem(
+        'guestToken'
+      )}/rated/movies?api_key=297a876b5d898a21bdf4174dff371b61&language=en-US&sort_by=created_at.asc&page=${page}`
+    )
+    const { results, total_pages } = await result.json()
+    return { results: results, totalPagesRanked: total_pages }
+  }
+
+  async getResource(page) {
+    const result = await fetch(
+      `https://api.themoviedb.org/3/trending/movie/week?api_key=297a876b5d898a21bdf4174dff371b61&page=${page}`
+    )
+    if (!result.ok) {
+      render(<Alert message="Error" description="Error with server." type="error" showIcon />)
+    }
+    const body = await result.json()
+    return { elements: body.results, maxPage: body.total_pages }
+  }
+  async getGenreList() {
+    const result = await fetch(
+      'https://api.themoviedb.org/3/genre/movie/list?api_key=297a876b5d898a21bdf4174dff371b61&language=en-US'
+    )
+    if (!result.ok) {
+      render(<Alert message="Error" description="Error with server." type="error" showIcon />)
+    }
+    const body = await result.json()
+    return body.genres
+  }
+
+  async getFilmBySearching(value, page) {
+    const result = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=297a876b5d898a21bdf4174dff371b61&language=en-US&page=1&include_adult=false&query=${value}&page=${page}`
+    )
+    if (!result.ok) {
+      render(
+        <Alert
+          message="Error"
+          description={`Nothing was found according to your request ${this}.`}
+          type="error"
+          showIcon
+        />
+      )
+    }
+    const body = await result.json()
+    return { elements: body.results, maxPage: body.total_pages }
+  }
+}
